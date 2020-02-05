8f59d886c615d6e44fe5988ecb6cd73291ecce78
Natsumi <retrogreensnake@gmail.com>
2020-02-05 12:08:57 +0200

    documented installing AMPS

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 "AMPS\Includer.exe" ASM68K AMPS AMPS\.Data
-asm68k /p /m /j ../* code/main.asm, player.md, , .lst>.log
+asm68k /p /m code/main.asm, player.md, , .lst>.log
 type .log
 if not exist player.md pause & exit
 error\convsym .lst player.md -input asm68k_lst -inopt "/localSign=. /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
