783d5052fc29af2ef79f0d8ce46922e2e6f54e32
fuzzy <ezra@insicuri.net>
2022-11-15 17:02:28 -0500

    Delete AMPS

--- AMPS/dac/Compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set MAINDIR=%CD%
-set COMPDIR="C:\MinGW\mingw32\bin"
-cd /d %COMPDIR%
-
-g++.exe -static-libgcc -static-libstdc++ "%MAINDIR%\ConvPCM.c" -o "%MAINDIR%\ConvPCM.exe"
-
-pause
